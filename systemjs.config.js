8100aae655c1696778e9729233b44d983cb1a87e
Sumit Gupta <sumit@apache.org>
2016-09-16 14:39:02 -0400

    Moved apps folder

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,8 +10,8 @@
     },
     // map tells the System loader where to look for things
     map: {
-      // our app is within the app folder
-      app: 'app',
+      // our app is within the apps folder
+      app: 'apps',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
